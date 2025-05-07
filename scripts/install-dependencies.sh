78d0aa8886b9fc37f8c0458a774e5568f8b59b28
brokenlander <94259413+brokenlander@users.noreply.github.com>
2025-05-06 21:49:45 -0400

    Update install-dependencies.sh

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -89,6 +89,13 @@ export PATH="$HOME/.local/bin:$PATH"
 
 # NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | PROFILE=~/.zshrc bash
+
+# Source NVM immediately so it's available in the same script
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Install Node.js
 nvm install --lts
 nvm use --lts
 
